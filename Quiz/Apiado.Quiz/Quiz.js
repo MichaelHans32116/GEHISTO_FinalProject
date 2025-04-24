789115b2c91da7ba48777c70c9be17b137c5b479
Liooouu <apiadojohnliol@gmail.com>
2025-04-24 19:07:02 +0800

    Fixed Quiz

--- Quiz/Apiado.Quiz/Quiz.js
+++ Quiz/Apiado.Quiz/Quiz.js
@@ -1,81 +1,79 @@
-document.getElementsByClassName('container')[0].style.display = "block";
-
-function next(id) {
-    var correctAnswer = document.getElementById('correct' + id);
-
-    if (correctAnswer.checked) {
-        
+window.onload = function () {
+    const containers = document.getElementsByClassName('container');
+    containers[0].style.display = "block";
+    containers[0].style.opacity = 1;
+  };
+  
+  function goBack() {
+    window.location.href = "Front.html";
+  }
+  
+    function next(id) {
+      const containers = document.getElementsByClassName('container');
+      const correctAnswer = document.getElementById('correct' + id);
+    
+      const transitionToNext = () => {
+        containers[id - 1].style.opacity = 0;
+    
+        setTimeout(() => {
+          containers[id - 1].style.display = "none";
+          containers[id].style.display = "block";
+          containers[id].style.opacity = 1;
+        }, 500);
+      };
+    
+      if (correctAnswer.checked) {
         Swal.fire({
-            title: "Correct!",
-            text: "Great job!",
-            icon: "success",
-            confirmButtonText: "Next"
-        }).then(() => {
-            
-            document.getElementsByClassName('container')[id - 1].style.display = "none";
-            document.getElementsByClassName('container')[id].style.display = "block";
-        });
-    } else {
-        
+          title: "Correct!",
+          text: "Great job!",
+          icon: "success",
+          confirmButtonText: "Next"
+        }).then(transitionToNext);
+      } else {
         Swal.fire({
-            title: "Oops!",
-            text: "That’s not correct.",
-            icon: "error",
-            confirmButtonText: "Try next"
-        }).then(() => {
-            document.getElementsByClassName('container')[id - 1].style.display = "none";
-            document.getElementsByClassName('container')[id].style.display = "block";
-        });
+          title: "Oops!",
+          text: "That’s not correct.",
+          icon: "error",
+          confirmButtonText: "Next"
+        }).then(transitionToNext);
+      }
     }
-}
-
-function result() {
-    var score = 0;
-    for (let i = 1; i <= 10; i++) {
-        if (document.getElementById('correct' + i).checked) {
-            score++;
+    
+    function result() {
+      let score = 0;
+      for (let i = 1; i <= 10; i++) {
+        if (document.getElementById('correct' + i)?.checked) {
+          score++;
         }
-    }
-
-    Swal.fire({
+      }
+    
+      Swal.fire({
         title: "Quiz Complete!",
         text: "Your score is: " + score + "/10",
         icon: "info",
-        confirmButtonText: "Okay"
-    });
-}
-
-function result() {
-    var score = 0;
-    if(document.getElementById('correct1').checked) {
-        score++;
+        showCancelButton: true,
+        confirmButtonText: "Restart Quiz",
+        cancelButtonText: "Close",
+        confirmButtonColor: "#4CAF50",
+        cancelButtonColor: "#d33"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "Front.html";
+        }
+      });
     }
-    if(document.getElementById('correct2').checked) {
-        score++;
+  
+    function restartQuiz() {
+      
+      const radios = document.querySelectorAll('input[type="radio"]');
+      radios.forEach(radio => radio.checked = false);
+    
+      const containers = document.getElementsByClassName('container');
+      for (let i = 1; i < containers.length; i++) {
+        containers[i].style.display = "none";
+        containers[i].style.opacity = 0;
+      }
+    
+      containers[1].style.display = "block";
+      containers[1].style.opacity = 1;
     }
-    if(document.getElementById('correct3').checked) {
-        score++;
-    }
-    if(document.getElementById('correct4').checked) {
-        score++;
-    }
-    if(document.getElementById('correct5').checked) {
-        score++;
-    }
-    if(document.getElementById('correct6').checked) {
-        score++;
-    }
-    if(document.getElementById('correct7').checked) {
-        score++;
-    }
-    if(document.getElementById('correct8').checked) {
-        score++;
-    }
-    if(document.getElementById('correct9').checked) {
-        score++;
-    }
-    if(document.getElementById('correct10').checked) {
-        score++;
-    }
-    alert("your score is: "+ score);
-}

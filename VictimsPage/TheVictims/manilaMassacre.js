fdc325643b32454d6d62dab2f83d0bb7a68fa8b1
Michael Hans Magbojos <magbojosmichaelhans@gmail.com>
2025-05-14 10:00:33 +0800

    Fixed fire

--- VictimsPage/TheVictims/manilaMassacre.js
+++ VictimsPage/TheVictims/manilaMassacre.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const spawnFire = () => {
         const fire = document.createElement("img");
-        fire.src = "../Images/fire.png";
+        fire.src = "../Images/Fire.png";
         fire.classList.add("fire");
 
         // Random position
